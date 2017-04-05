5495461605e8b5e6c67864d100b2ffb78b153741
Don Draper <catinahat@Dons-iMac.local>
2017-04-04 20:47:52 -0700

    got rid of notification toggle

--- CoreApplicationPrototype/SettingsViewController.swift
+++ CoreApplicationPrototype/SettingsViewController.swift
@@ -26,7 +26,7 @@ class SettingsViewController: UIViewController {
     
     /************ View Outlets **********/
     
-    @IBOutlet weak var NotificationLabel: UILabel!
+
     
     @IBOutlet weak var logButton: UIButton!
     
@@ -34,16 +34,7 @@ class SettingsViewController: UIViewController {
 
     /************ View Actions **********/
 
-    @IBAction func toggleNotifications(_ sender: UISwitch) {
-        if (sender.isOn)    {
-            NotificationLabel.text = "Notifications: On"
-            notificationsOn = true
-        }
-        else {
-            NotificationLabel.text = "Notifications: Off"
-            notificationsOn = false
-        }
-    }
+
     
     
     // This function operates differently depending on the user login status.
@@ -123,12 +114,6 @@ class SettingsViewController: UIViewController {
     // and logButton and acctMod button to the correct title depending on login status.
     override func viewDidLoad() {
         super.viewDidLoad()
-        if(notificationsOn){
-            NotificationLabel.text = "Notifications: On"
-        }
-        else{
-            NotificationLabel.text = "Notifications: Off"
-        }
         
         // Fetch the user to check login status
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
