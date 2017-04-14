39cb89b133db153b46efa8073cfe3b4fcc7bffde
Paul <paul@Pauls-iMac.local>
2017-04-14 12:32:37 -0700

    workworkwork

--- CoreApplicationPrototype/WalletViewController.swift
+++ CoreApplicationPrototype/WalletViewController.swift
@@ -26,11 +26,39 @@ class WalletViewController: UITableViewController{
     @IBOutlet var walletTableView: UITableView!
     
     let cellID = "WalletCell"
-    var indexPath = -1
+    var selectedIndexPath = -1
     private var rewardArray = [Reward]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rewardArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let dequeued = tableView.dequeueReusableCell(withIdentifier: cellID, for: <#T##IndexPath#>)
+        
+        let cell = dequeued as! WalletTableViewCell
+        
+        cell.rewardTitle.text = rewardArray[indexPath.row].title
+        cell.rewardDes.text = rewardArray[indexPath.row].des
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if(selectedIndexPath == indexPath.row){
+            selectedIndexPath = -1
+        }
+        else{
+            selectedIndexPath = indexPath.row
+        }
+        
+        tableView.reloadRows(at: <#T##[IndexPath]#>, with: <#T##UITableViewRowAnimation#>)
     }
     
     
