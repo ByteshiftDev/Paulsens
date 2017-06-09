f58c1f50ddcc4378cece1338245342599906f75c
Cher Moua <mouacher@pdx.edu>
2017-06-09 14:20:59 -0700

    Testing editUser

--- CoreApplicationPrototype/WebCallController.swift
+++ CoreApplicationPrototype/WebCallController.swift
@@ -847,7 +847,7 @@ class WebCallController: URLSession {
         // Call the PATCH function to send data to web server telling it to alter that entry in the user table
         // Catch the response
         var toReturn: (Bool, String) = (true, "There was an error catching the response from the web server.")
-        patchRequest(urlToCall: SERVER_HOST_URL + "/account", data: data) { (dataJson) in
+        putRequest(urlToCall: SERVER_HOST_URL + "/account.json", data: data) { (dataJson) in
             print(dataJson["errors"] as Any)
             if let error = dataJson["errors"] as? Dictionary<String, [String]>{
                 print(error)
